93fef5ebc959464a38e3e30ed5caae5ec5f5c289
samirjabib <samirdev9669@gmail.com>
2022-12-11 14:20:54 -0500

    add

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -17,7 +17,7 @@ export const SideBar = ({ drawerWidth = 240 }) => {
         >
             <Toolbar>
                 <Typography variant='h6' noWrap component='div'>
-                    Fernando Herrera
+                    Samir Jabib
                 </Typography>
             </Toolbar>
             <Divider />
