221472b0a7c3317abb3610f8084aad011db09096
samirjabib <samirdev9669@gmail.com>
2022-12-12 00:30:23 -0500

    add thunk to login with google

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -2,17 +2,18 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { firebaseAuth } from "./config";
 
 
-const googleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider(); //Creamos una nueva instancia.
 
 
 export const signInWithGoogle = async() => {
     try{
-        const result = await signInWithPopup( firebaseAuth, googleProvider);
+        const result = await signInWithPopup( firebaseAuth, googleProvider); //Pasamos la auth y el proveedor para generar nuestro token, y llamaremos al popup con este metodo. 
 
         console.log(result)
 
         return {
             status:'success',
+            result
         }
     } catch(error){
         const errorCode = error.errorCode;
@@ -23,5 +24,4 @@ export const signInWithGoogle = async() => {
             errorMessage
         }
     }
-
-}
+};
---
+++ src/store/auth/thunks.js
@@ -0,0 +1,24 @@
+import { signInWithGoogle } from "../../firebase";
+import { onCheckingCredentials, onLogout, onLogin } from "./authSlice";
+
+
+export const checkingAuthentication = () => {
+    return async( dispatch) => {
+        dispatch(onCheckingCredentials());
+    }
+};
+
+
+export const startGoogleSignIn = () => {
+    return async( dispatch ) => {
+        dispatch(onCheckingCredentials());
+
+        const result = await signInWithGoogle();
+        if(result.status === 'failed') return dispatch(onLogout(result.errorMessage));
+
+        dispatch( onLogin(result));
+        console.log(result)
+    };
+};
+
+
